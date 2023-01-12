fa9a41d61b841cffbf76670620bccc9e283d2a1b
DJFP <djimenez@solusoft.es>
2023-01-12 12:47:14 +0100

    Changed package.json to aim this repository. Remove ignored files.

--- build/esm-debug/transports/decoderBase.d.ts
+++
@@ -1,5 +0,0 @@
-import { type Packet } from "engine.io-parser";
-export declare abstract class Decoder {
-    constructor();
-    decodePacket(data: any, binaryType: any): Packet;
-}
--- build/esm/transports/decoderBase.js
+++
@@ -1,7 +0,0 @@
-export class Decoder {
-    constructor() {
-    }
-    decodePacket(data, binaryType) {
-        throw "There is no decoder implemented";
-    }
-}
